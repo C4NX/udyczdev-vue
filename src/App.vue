eff20e7ddad3e4d14ea3c4e6fbaed349a6a4e9b7
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-08-12 12:41:04 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <WindowFrame title="test a">
+    <p class="text-gray-200">
+      test b
+    </p>
+  </WindowFrame>
+</template>
+
+<script setup lang="ts">
+import WindowFrame from './components/WindowFrame.vue';
+import './assets/main.css'
+
+</script>
---
+++ src/components/WindowFrame.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    defineProps<{
+        title: string
+    }>()
+</script>
+
+<template>
+    <div>
+        <p class="text-xl">
+            {{ title }}
+        </p>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
