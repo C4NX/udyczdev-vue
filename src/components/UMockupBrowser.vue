d6064548be2632ff3986c4992f2ee654697fb100
Udycz Alexis <udycz.alexis@gmail.com>
2023-09-02 22:02:45 +0200

    feat: responsive for the mockup browser

--- src/components/UMockupBrowser.vue
+++ src/components/UMockupBrowser.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="mockup-browser border bg-base-300 max-w-6xl">
-    <div class="mockup-browser-toolbar">
+    <div class="mockup-browser-toolbar !hidden sm:!inline-flex">
       <div class="input select-none">
         {{ title }}
       </div>
+    </div>
+    <div class="flex items-center w-100 justify-center m-4 sm:absolute sm:right-3 sm:top-3 sm:m-0">
       <ULocaleSwitcher />
     </div>
     <slot />
